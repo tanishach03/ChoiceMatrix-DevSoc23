ccf1a54fb61e605cdb5345bd3ac82df78a3556b9
Tanisha Choudhary <94930302+tanishach03@users.noreply.github.com>
2023-06-03 00:37:22 +0530

    Quarterly ratings

--- backenduser/index.js
+++ backenduser/index.js
@@ -21,23 +21,23 @@ const projectData = JSON.parse(
 );
 
 const feedbackSchema = new mongoose.Schema({
+  // projectId: {
+  //   type: String,
+  //   required: true,
+  // },
   departmentId: {
     type: String,
     required: true,
   },
-  projectId: {
-    type: String,
-    required: true,
-  },
   ratings: {
-    performance: Number,
-    ui_ux: Number,
-    pricing: Number,
-    support: Number,
+    performance: { type: Number },
+    ui_ux: { type: Number },
+    pricing: { type: Number },
+    support: { type: Number },
   },
 });
 
-const Feedback = mongoose.model("Fedback", feedbackSchema);
+const Feedback = mongoose.model("Feedback", feedbackSchema);
 
 // const login = (req, res) => {};
 
@@ -55,24 +55,43 @@ const Feedback = mongoose.model("Fedback", feedbackSchema);
 
 // app.get("/purchases");
 
-app.post("/purchases/:id", async (req, res) => {
+app.post("/purchases/:userId/:projectId", async (req, res) => {
   const feedbackData = req.body;
+  console.log(feedbackData);
   try {
-    const feedbackDocuments = [];
-    for (projects of feedbackData.projects) {
-      const { departmentId, projectId } = projectData;
-      const ratings = req.body.ratings[projectId];
-
-      const newFeedback = new Feedback({
-        departmentId,
-        projectId,
-        ratings,
-      });
-
-      feedbackDocuments.push(newFeedback);
+    const prevAvgRatings = [4, 3, 4.2, 3.5];
+    const currentAvgRatings = [];
+    const quarterRatings = [];
+    let sumPerformance = 0;
+    let sumUIUX = 0;
+    let sumPricing = 0;
+    let sumSupport = 0;
+    let avgPerformance = 0;
+    let avgUIUX = 0;
+    let avgPricing = 0;
+    let avgSupport = 0;
+    let l = feedbackData.length;
+    for (object in feedbackData) {
+      const { performance, ui_ux, pricing, support } =
+        feedbackData[object].ratings;
+      sumPerformance += performance;
+      sumUIUX += ui_ux;
+      sumPricing += pricing;
+      sumSupport += support;
+      avgPerformance = sumPerformance / l;
+      avgUIUX = sumUIUX / l;
+      avgPricing = sumPricing / l;
+      avgSupport = sumSupport / l;
     }
-
-    await Feedback.insertMany(feedbackDocuments);
+    currentAvgRatings.push(avgPerformance);
+    currentAvgRatings.push(avgUIUX);
+    currentAvgRatings.push(avgPricing);
+    currentAvgRatings.push(avgSupport);
+    for (let i = 0; i < 4; i++) {
+      quarterRatings.push(currentAvgRatings[i] - prevAvgRatings[i]);
+    }
+    console.log(quarterRatings);
+    await Feedback.insertMany(feedbackData);
     res.status(200).json({ message: "Feedback submitted successfully" });
   } catch (err) {
     console.log("Error saving feedback to the database: ", err);
